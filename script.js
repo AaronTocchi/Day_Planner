16a6f5fb4d7b4b3662064f78322e0aba6d1c300b
Toki <actocchi@gmail.com>
2019-09-28 20:06:27 -0700

    added day to date

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-let date = moment().format('MMMM Do YYYY');
 
-console.log(date);
-$("#current-day").text(date);
+// moment.js variables
+let date = moment().format('MMMM Do YYYY');
+let day = moment().format("dddd")
+let taskArray =[]
+
+
+// my header including date
+$("#current-day").text(day + ", " + date);
+
+saveTask() {
+}
