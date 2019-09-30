f76b85240b23bee87db57782029e5cac166f6fee
Toki <actocchi@gmail.com>
2019-09-30 10:01:59 -0700

    save key to local storage working for each field

---
+++ blank.js
@@ -0,0 +1,11 @@
+
+let object = {
+    '0': 'hello',
+    time: 'goodbye'
+  }
+ 
+  console.log(object);
+
+  let time = '0';
+  console.log(object.time); // goodbye
+  console.log(object[time]); // hello
--- script.js
+++ script.js
@@ -3,9 +3,8 @@ let date = moment().format('MMMM Do YYYY');
 let day = moment().format('dddd');
 let saveButtons = $('.btn');
 // use querySelector instead of jQuery.
-let input = document.querySelector('.form-control');
 let currentHour = moment().hour();
-let taskArray = [];
+let taskArray = getLocalStorage();
 
 // my header including date
 $('#current-day').text(day + ', ' + date);
@@ -22,25 +21,65 @@ for (i = 9; i < 18; i++) {
 }
 
 // saveTask(){
-//     event.preventDefault()
+  //     event.preventDefault()
+  
+  // itemsArray.push(saveButtons.value);
+  // localStorage.setItem('items', JSON.stringify(taskArray));
+  
+  // };
+  // submit button to save to local storage
+  saveButtons.on('click', function (e) {
+    console.log(this);
+    let time = $(this).attr("id");
+    let value = $( '#' + time ).val(); // "id='text-9'"
+  // let value = input.value;
 
-// itemsArray.push(saveButtons.value);
-// localStorage.setItem('items', JSON.stringify(taskArray));
+  setLocalStorage(time, value);
+  
 
-// };
-// submit button to save to local storage
-saveButtons.on('click', function(e) {
-  const value = input.value;
-  let taskArray = [];
-
-  taskArray.push(value);
-  localStorage.setItem('items', JSON.stringify(taskArray));
-  console.log('hello world');
 });
 // loads data from storage
 function loadTasks() {
   let data = JSON.parse(localStorage.getItem('items'));
   console.log(data);
+
 }
 // run on page load
 loadTasks();
+
+function getLocalStorage() {
+  return JSON.parse(localStorage.getItem('items'));
+
+}
+
+function setLocalStorage(time, value){
+  console.log(time,value);
+  // If 'items' exists in LS
+  if (taskArray) {
+    // Loop over taskArray to either update or set new task 
+    // .forEach() is a for loop that runs a function for each element in array (just like the code block for each iteration in a for loop) 
+    taskArray.forEach(function(currentTask) {
+      // if task's property/key exists i.e. === 9 then reset value
+      // else create new task object and push to taskArray
+      if (currentTask[time]) {
+        currentTask[time] = value
+      } else if (!currentTask[time]) {
+        let newTask = {};
+        newTask[time] = value;
+        taskArray.push(newTask);
+        console.log(newTask, taskArray);
+      }
+    });
+  
+    // else if nothing saved in LS (i.e. null or does NOT (!) exist or is NOT true)
+  } else if (!taskArray) {
+    taskArray = [];
+    // set taskArray to empty array
+    let newTask = {};
+    newTask[time] = value;
+    taskArray.push(newTask);
+  }
+    localStorage.setItem('items', JSON.stringify(taskArray));
+  };
+
+
